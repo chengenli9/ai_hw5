f9fd824a5665784a45954947f0e4af779c123fdc
Chengen Li <chengen2828@gmail.com>
2025-11-04 12:50:11 -0800

    working training

--- hw5/src/AI/freytag26_lic27_hw5.py
+++ hw5/src/AI/freytag26_lic27_hw5.py
@@ -1,16 +1,6 @@
-import random
-import sys
-sys.path.append("..")  #so other modules can be found in parent dir
-from Player import *
-from Constants import *
-from Construction import CONSTR_STATS
-from Ant import UNIT_STATS
-from Move import Move
-from GameState import *
-from AIPlayerUtils import *
 import numpy as np
+import random
 
-## ------------- Neural Network ---------------
 
 INPUT_NODES = 4
 HIDDEN_NODES = 8
@@ -51,14 +41,14 @@ def sigmoid(x):
 def sigmoidDerivative(x):
     return x * (1.0 - x)
 
-# ---- output function ----
+# forward propagation 
 def forwardPass(inputs, hiddenLayerWeights, outputLayerWeights):
     # add bias to input
     inputWithBias = np.append(inputs, 1).reshape(1, -1)
 
     # hidden layer
     hiddenInput = np.dot(inputWithBias, hiddenLayerWeights)
-    hiddenOutput = sigmoid(hiddenInput)
+    hiddenOutput = sigmoid(hiddenInput) 
 
     # add bias to hidden layer
     hiddenLayerWithBias = np.append(hiddenInput, 1).reshape(1, -1)
@@ -67,30 +57,39 @@ def forwardPass(inputs, hiddenLayerWeights, outputLayerWeights):
     finalInput = np.dot(hiddenLayerWithBias, outputLayerWeights)
     finalOutput = sigmoid(finalInput)
 
-
+    # 8 hiddenOutput for every 1 finalOutput
     return finalOutput, hiddenOutput 
 
 
+# back propagation
+def backwardPass(inputs, target, finalOutput, hiddenOutput,
+                 hiddenLayerWeights, outputLayerWeights):
+    # Convert to 2D arrays 
+    inputs = inputs.reshape(1, -1)
+    hiddenOutput = hiddenOutput.reshape(1, -1)
+    target = np.array(target).reshape(1, -1)
 
-def backwardPass(inputs, hiddenLayerWeights, outputLayerWeights, target):
-    # error calculation
-    finalOutput, hiddenOutput = forwardPass(inputs, hiddenLayerWeights, outputLayerWeights)
-    outputError = np.array(target - finalOutput)
-    hiddenErrorTerms = np.dot(outputError, sigmoidDerivative(finalOutput))
-    hiddenError = np.array(hiddenErrorTerms * hiddenLayerWeights)
+    # Add bias to inputs and hidden layer
+    inputWithBias = np.append(inputs, 1).reshape(1, -1)
+    hiddenWithBias = np.append(hiddenOutput, 1).reshape(1, -1)
 
-    # adjusted weights
-    adjustedOutputLayerWeights = []
-    for i in range(outputLayerWeights):
-        adjustedOutputLayerWeights.append(outputLayerWeights[i] + LEARNING_RATE * outputError * sigmoidDerivative(finalOutput) * hiddenOutput[i])
+    # Calculate output error and delta 
+    outputError = target - finalOutput
+    outputDelta = outputError * sigmoidDerivative(finalOutput)
 
-    adjustedHiddenLayerWeights = []
-    for i in range(hiddenLayerWeights):
-        adjustedHiddenLayerWeights.append(hiddenLayerWeights[i] + LEARNING_RATE * hiddenError * inputs[i%8])
+    # Calculate hidden layer error and delta 
+    hiddenError = outputDelta.dot(outputLayerWeights[:-1].T)
+    hiddenDelta = hiddenError * sigmoidDerivative(hiddenOutput)
 
-    return np.array(adjustedOutputLayerWeights), np.array(adjustedHiddenLayerWeights)
+    # Update the weights 
+    outputLayerWeights += LEARNING_RATE * hiddenWithBias.T.dot(outputDelta)
+    hiddenLayerWeights += LEARNING_RATE * inputWithBias.T.dot(hiddenDelta)
+
+    return hiddenLayerWeights, outputLayerWeights, abs(outputError[0][0])
 
 
+
+# ----- Training the NN -----
 
 # init training round
 epoch = 0
@@ -109,143 +108,17 @@ while True:
         # forward pass
         finalOutput, hiddenOutput = forwardPass(inputs, hiddenLayerWeights, outputLayerWeights)
 
-        # ---- to be continued ----
-        # add backpropagation 
+        # update the weights using backpropagation 
+        hiddenLayerWeights, outputLayerWeights, error = backwardPass(
+            inputs, target, finalOutput, hiddenOutput,
+            hiddenLayerWeights, outputLayerWeights
+        )
+        # add to the list of errors
+        errors.append(error) 
 
-    avgError = np.mean(errors)
+    averageError = np.mean(errors)
+    print(f"Epoch {epoch}: average error = {averageError:.4f}")
 
-    if avgError < 0.5:
+    # stop the loop when average error is below 0.05
+    if averageError < 0.05:
         break
-
-
-
-
-
-        
-        
-
-
-
-
-
-
-
-##
-#AIPlayer
-#Description: The responsbility of this class is to interact with the game by
-#deciding a valid move based on a given game state. This class has methods that
-#will be implemented by students in Dr. Nuxoll's AI course.
-#
-#Variables:
-#   playerId - The id of the player.
-##
-class AIPlayer(Player):
-
-    #__init__
-    #Description: Creates a new Player
-    #
-    #Parameters:
-    #   inputPlayerId - The id to give the new player (int)
-    #   cpy           - whether the player is a copy (when playing itself)
-    ##
-    def __init__(self, inputPlayerId):
-        super(AIPlayer,self).__init__(inputPlayerId, "Big Brain Bot")
-    ##
-    #getPlacement
-    #
-    #Description: called during setup phase for each Construction that
-    #   must be placed by the player.  These items are: 1 Anthill on
-    #   the player's side; 1 tunnel on player's side; 9 grass on the
-    #   player's side; and 2 food on the enemy's side.
-    #
-    #Parameters:
-    #   construction - the Construction to be placed.
-    #   currentState - the state of the game at this point in time.
-    #
-    #Return: The coordinates of where the construction is to be placed
-    ##
-    def getPlacement(self, currentState):
-        numToPlace = 0
-        #implemented by students to return their next move
-        if currentState.phase == SETUP_PHASE_1:    #stuff on my side
-            numToPlace = 11
-            moves = []
-            for i in range(0, numToPlace):
-                move = None
-                while move == None:
-                    #Choose any x location
-                    x = random.randint(0, 9)
-                    #Choose any y location on your side of the board
-                    y = random.randint(0, 3)
-                    #Set the move if this space is empty
-                    if currentState.board[x][y].constr == None and (x, y) not in moves:
-                        move = (x, y)
-                        #Just need to make the space non-empty. So I threw whatever I felt like in there.
-                        currentState.board[x][y].constr == True
-                moves.append(move)
-            return moves
-        elif currentState.phase == SETUP_PHASE_2:   #stuff on foe's side
-            numToPlace = 2
-            moves = []
-            for i in range(0, numToPlace):
-                move = None
-                while move == None:
-                    #Choose any x location
-                    x = random.randint(0, 9)
-                    #Choose any y location on enemy side of the board
-                    y = random.randint(6, 9)
-                    #Set the move if this space is empty
-                    if currentState.board[x][y].constr == None and (x, y) not in moves:
-                        move = (x, y)
-                        #Just need to make the space non-empty. So I threw whatever I felt like in there.
-                        currentState.board[x][y].constr == True
-                moves.append(move)
-            return moves
-        else:
-            return [(0, 0)]
-    
-    ##
-    #getMove
-    #Description: Gets the next move from the Player.
-    #
-    #Parameters:
-    #   currentState - The state of the current game waiting for the player's move (GameState)
-    #
-    #Return: The Move to be made
-    ##
-    def getMove(self, currentState):
-        moves = listAllLegalMoves(currentState)
-        selectedMove = moves[random.randint(0,len(moves) - 1)];
-
-        #don't do a build move if there are already 3+ ants
-        numAnts = len(currentState.inventories[currentState.whoseTurn].ants)
-        while (selectedMove.moveType == BUILD and numAnts >= 3):
-            selectedMove = moves[random.randint(0,len(moves) - 1)];
-            
-        return selectedMove
-    
-    ##
-    #getAttack
-    #Description: Gets the attack to be made from the Player
-    #
-    #Parameters:
-    #   currentState - A clone of the current state (GameState)
-    #   attackingAnt - The ant currently making the attack (Ant)
-    #   enemyLocation - The Locations of the Enemies that can be attacked (Location[])
-    ##
-    def getAttack(self, currentState, attackingAnt, enemyLocations):
-        #Attack a random enemy.
-        return enemyLocations[random.randint(0, len(enemyLocations) - 1)]
-
-    ##
-    #registerWin
-    #
-    # This agent doens't learn
-    #
-    def registerWin(self, hasWon):
-        #method templaste, not implemented
-        pass
-
-
-
-
